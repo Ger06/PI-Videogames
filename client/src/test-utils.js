5f8ac8523fdded4b053783d8c74f57ab4af79557
Ger06 <gerardo.kohatus@gmail.com>
2021-11-01 11:16:56 -0300

    Update test-utils.js

--- client/src/test-utils.js
+++ client/src/test-utils.js
@@ -1,27 +1,27 @@
-// test-utils.jsx
-import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-// Import your own reducer
-import {rootReducer} from './reducer/index';
-import CreateGame from './components/CreateGame/CreateGame';
+// // test-utils.jsx
+// import React from 'react';
+// import { render as rtlRender } from '@testing-library/react';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { Provider } from 'react-redux';
+// // Import your own reducer
+// import {rootReducer} from './reducer/index';
+// import CreateGame from './components/CreateGame/CreateGame';
 
-function render(
-  ui,
-  {
-    preloadedState,
-    store = configureStore({ reducer: rootReducer, preloadedState }),
-    ...renderOptions
-  } = {}
-) {
-  function Wrapper( CreateGame ) {
-    return <Provider store={store}>{CreateGame}</Provider>
-  }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
-}
+// function render(
+//   ui,
+//   {
+//     preloadedState,
+//     store = configureStore({ reducer: rootReducer, preloadedState }),
+//     ...renderOptions
+//   } = {}
+// ) {
+//   function Wrapper( CreateGame ) {
+//     return <Provider store={store}>{CreateGame}</Provider>
+//   }
+//   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
+// }
 
-// re-export everything
-export * from '@testing-library/react'
-// override render method
-export { render }
+// // re-export everything
+// export * from '@testing-library/react'
+// // override render method
+// export { render }
